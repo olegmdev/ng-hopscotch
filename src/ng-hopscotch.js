d71087fdcc5645027a44a50476196f9e054706ba
Oleg Meleshko <qu1ze34@gmail.com>
2015-12-29 17:44:10 +0200

    switched it to service

--- src/ng-hopscotch.js
+++ src/ng-hopscotch.js
@@ -8,6 +8,57 @@ angular.module('ngHopscotch').factory('hopscotch', function() {
   return window.hopscotch;
 });
 
-angular.module('ngHopscotch').factory('HSTour', ['hopscotch', function(hopscotch) {
-  return hopscotch;
+angular.module('ngHopscotch').factory('HSHelper', function() {
+
+  var helper = {
+    extend : function(child, parent) {
+      for (var key in parent) {
+        if (parent.hasOwnProperty(key)) {
+          child[key] = parent[key];
+        }
+      }
+    }
+  };
+
+  return helper;
+});
+
+angular.module('ngHopscotch').service('HSTour', ['hopscotch', 'HSHelper', function(hopscotch, HSHelper) {
+
+  function HSTour(tour) {
+    this.tour = tour;
+  }
+
+  HSTour.prototype.init = function(tour) {
+    this.tour = tour;
+  };
+
+  HSTour.prototype.start = function() {
+    hopscotch.startTour(this.tour);
+  };
+
+  HSTour.prototype.bind = function(options) {
+    for (var key in options) {
+      var step = this.tour.steps.find(function(item) { return item.target === key; });
+      if (step) {
+        HSHelper.extend(step, options[key]);
+      }
+    }
+  };
+
+  HSTour.prototype.removeStep = function(step) {
+    switch(typeof step) {
+      case 'number':
+        delete this.tour.steps[step];
+        break;
+      case 'string':
+        this.tour.steps.splice(this.tour.steps.findIndex(function(item) {
+          return item.target === step;
+        }), 1);
+      case 'object':
+        break;
+    }
+  };
+
+  return HSTour;
 }]);
